106707137240b3130b6f4f2cf60f79a4bd999a9d
郑龙飞 <longfeizheng@users.noreply.github.com>
2020-03-09 09:57:26 +0800

    Update ThreadPool.java

--- _drafts/ThreadPool.java
+++ _drafts/ThreadPool.java
@@ -1,11 +1,27 @@
 public class Main {
 
-    private static ThreadPoolExecutor poolTest = new ThreadPoolExecutor(3, 3, 10, TimeUnit.MINUTES, new LinkedBlockingQueue<>(50));
+    private static ThreadPoolExecutor poolTest = new ThreadPoolExecutor(2, 3, 10, TimeUnit.MINUTES, new LinkedBlockingQueue<>(50));
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             poolTest.execute(new TestRunnable("test-----" + i));
         }
+//        poolTest.shutdown();
+
+        for (int i = 10; i < 20; i++) {
+            poolTest.execute(new TestRunnable("test-----" + i));
+        }
+        System.out.println("测试线程池");
+
+        long begin = System.currentTimeMillis();
+        while (true) {
+            if (System.currentTimeMillis() - begin > 2000) {
+                System.out.println("中断循环");
+                break;
+            }
+        }
+
+        System.out.println("测试线程池和中断循环");
     }
 }
 
